daa900d1402f94a1ea2d102c3583c29b38a7e1fc
Jansonboss <yetao5397@gmail.com>
2021-10-19 18:08:14 -0700

    add extra function on logger

--- logger.py
+++ logger.py
@@ -5,6 +5,12 @@ class myLogger(pl.LightningModule):
     
     def __init__(self):
         super().__init__()
+    
+    def loaded_optimizer_states_dict(self) -> dict:
+        return super().loaded_optimizer_states_dict
         
     def logger(self):
         return super().logger
+    
+
+def main()
