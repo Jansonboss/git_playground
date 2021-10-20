033a76b495856790e9125f69065d9cf6167e6008
Jansonboss <yetao5397@gmail.com>
2021-10-19 17:12:44 -0700

    added2 in property.py fix Typo: added myModel module in property

--- property.py
+++ property.py
@@ -1,4 +1,17 @@
 import pandas as pd
 from time import sleep
 
+
 import pytorch_lightning as pl 
+
+
+class myModel(pl.LightningModule):
+
+	def __init__(self):
+		super().__init__()
+	
+	def training_step(self):
+		pass
+
+	def validation_step(self, *args, **kwargs) -> Optional[STEP_OUTPUT]:
+		pass
